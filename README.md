c1109679805a323c5961962ec8272fa7179485b1
Phitrapy <clement.begnaud@gmail.com>
2022-09-02 18:45:15 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-6dxte8
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-6dxte8)
---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from './components/HelloWorld.vue';
+</script>
+
+<template>
+  <div>
+    <a href="https://github.com/phitrapy" target="_blank">
+      <img
+        src="https://avatars.githubusercontent.com/u/15626586?v=4"
+        class="logo"
+        alt="Vite logo"
+      />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+</style>
