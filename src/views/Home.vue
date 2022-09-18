558166bd456bfe9ed7459128e693dc4c51de1eae
Phitrapy <clement.begnaud@gmail.com>
2022-09-18 17:07:39 +0200

    redo layout

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,20 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div>
-    <a href="https://github.com/phitrapy" target="_blank">
-      <img
-        src="https://avatars.githubusercontent.com/u/15626586?v=4"
-        class="logo"
-        alt="Vite logo"
-      />
-    </a>
+  <div class="layout-home">
+    <div>
+      <a href="https://github.com/phitrapy" target="_blank">
+        <img
+          src="https://avatars.githubusercontent.com/u/15626586?v=4"
+          class="logo"
+          alt="Vite logo"
+        />
+      </a>
+    </div>
+    <h1>Components Playground</h1>
+
+    <router-link to="/separators">Separators</router-link>
   </div>
-  <h1>Components Playground</h1>
-  <!-- <HelloWorld msg="Vite + Vue" /> -->
 </template>
 
 <style scoped>
@@ -20,7 +23,15 @@
   padding: 1.5em;
   will-change: filter;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
+}
+
+.layout-home {
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 2rem;
+  text-align: center;
 }
 </style>
